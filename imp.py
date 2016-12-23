515b47ffdc9a335605890d72759861d4faefd133
dohsimpson <mydohsimpson@gmail.com>
2016-12-23 13:41:12 -0500

    Added import test file

---
+++ imp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# import p1 # this should print 1
+# import p1.run # this should also print 1
+# import p1; import p1 # this would only print 1 once
+# from p1 import run # this would also print 1
